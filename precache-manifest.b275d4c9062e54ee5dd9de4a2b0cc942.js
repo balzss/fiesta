90fe78cd13ba159d95ac6e01618676114c984e39
balazs.saros <balazs.saros@gmail.com>
2021-06-21 01:42:05 +0200

    Updates

---
+++ precache-manifest.b275d4c9062e54ee5dd9de4a2b0cc942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a398becb37a3ff9e9d6404c928418d5",
+    "url": "/fiesta/index.html"
+  },
+  {
+    "revision": "241f1f1ff5e295dd1849",
+    "url": "/fiesta/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "3dfdb9d0210118dfaeaf",
+    "url": "/fiesta/static/css/main.232993f6.chunk.css"
+  },
+  {
+    "revision": "241f1f1ff5e295dd1849",
+    "url": "/fiesta/static/js/2.ccb6dbb7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fiesta/static/js/2.ccb6dbb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dfdb9d0210118dfaeaf",
+    "url": "/fiesta/static/js/main.d1980eae.chunk.js"
+  },
+  {
+    "revision": "9f9b0c7a8d1e5fa9475c",
+    "url": "/fiesta/static/js/runtime-main.dbdeaa28.js"
+  }
+]);
