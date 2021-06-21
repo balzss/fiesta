98e6b0790e7fbac78f6dff29e3fca65c37936fc2
balazs.saros <balazs.saros@gmail.com>
2021-06-21 03:38:43 +0200

    remove extra stuff

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,10 @@ import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Host from "./Host";
 import Party from "./Party";
-import Join from "./Join";
-import HomePage from "./HomePage";
 const Router = () => (
   <HashRouter>
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/host" component={Host} />
-      <Route exact path="/join" component={Join} />
+      <Route exact path="/" component={Host} />
       <Route exact path="/party" component={Host} />
       <Route exact path="/party/:host_id" component={Party} />
     </Switch>
